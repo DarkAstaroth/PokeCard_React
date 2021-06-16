20f2509a50d0399f283c9cbe3169d076dc794c40
Manuel <mninahuanca4@gmail.com>
2021-06-15 23:30:21 -0400

    Adding Fancier images

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../css/Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) :number);
 
 const Pokecard = ({name,id,type,exp}) => {
-  let imgSrc=`${POKE_API}${id}.png`;
+  let imgSrc=`${POKE_API}${padToThree(id)}.png`;
   return(
     <div className="Pokecard">
       <h1 className="Pokecard-title">{name}</h1>
