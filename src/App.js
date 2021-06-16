4786a563bf0a9a57fd548ea2db4e8ba0b80976af
Manuel <mninahuanca4@gmail.com>
2021-06-15 23:24:25 -0400

    Creating a pokedex component and adding styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
+import Pokedex from './components/Pokedex';
 import './App.css';
-import Pokecard from './components/Pokecard';
 
 function App() {
   return (
     <div className="App">
-      <Pokecard 
-	id={4}
-	name="Charmander"
-	type="fire"
-	exp={62}
-      />
+      <Pokedex />
     </div>
   );
 }
--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,4 +1,4 @@
-import React , {Component} from 'react';
+import React from 'react';
 import '../css/Pokecard.css';
 
 const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
@@ -7,10 +7,10 @@ const Pokecard = ({name,id,type,exp}) => {
   let imgSrc=`${POKE_API}${id}.png`;
   return(
     <div className="Pokecard">
-      <h1>{name}</h1>
+      <h1 className="Pokecard-title">{name}</h1>
       <img src={imgSrc} alt=""/>
-      <div>Type:{type}</div>
-      <div>Exp:{exp}</div>
+      <div className="Pokecard-data">Type:{type}</div>
+      <div className="Pokecard-data">Exp:{exp}</div>
     </div>  
   ); 
 } 
